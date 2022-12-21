83da4f467ddd5c5c4a299d3d8ca48c1c036e1641
DESKTOP-CFOAIDA\rabbitdev <rabbitdev>
2022-12-22 04:23:29 +0900

    remove token

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,13 +1,11 @@
 import Axios from 'axios'
 import { getCookie } from 'xsrf-cookie';
-
-const xsrfToken  = getCookie();
  
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
-        'X-XSRF-TOKEN': xsrfToken 
+        
     },
     withCredentials: true,
 })
