fc86774589af8cf690f4618177fd574ed790a9f6
DESKTOP-CFOAIDA\rabbitdev <rabbitdev>
2022-12-22 03:40:05 +0900

    add token

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,11 +1,29 @@
 import Axios from 'axios'
 
+function getCookie(name) {
+    if (!document.cookie) {
+        return null;
+    }
+
+    const xsrfCookies = document.cookie.split(';')
+        .map(c => c.trim())
+        .filter(c => c.startsWith(name + '='));
+
+    if (xsrfCookies.length === 0) {
+        return null;
+    }
+    return decodeURIComponent(xsrfCookies[0].split('=')[1]);
+}
+
+const csrfToken = getCookie('CSRF-TOKEN');
+ 
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-TOKEN': csrfToken
     },
-    withCredentials: false,
+    withCredentials: true,
 })
 
 export default axios
