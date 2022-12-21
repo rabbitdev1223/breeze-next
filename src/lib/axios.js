694a2e27daa10249d4e602a5d6a990cc07c45ffa
DESKTOP-CFOAIDA\rabbitdev <rabbitdev>
2022-12-22 01:56:58 +0900

    restore

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: "https://smart-rabbit-dev.eu-1.sharedwithexpose.com",
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
