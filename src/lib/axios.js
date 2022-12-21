382390fc3a385953565f27f90f3dc93e2c3ee821
DESKTOP-CFOAIDA\rabbitdev <rabbitdev>
2022-12-22 03:57:46 +0900

    update token

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 import { getCookie } from 'xsrf-cookie';
 
-const xsrfToken  = getCookie('XSRF-TOKEN');
+const xsrfToken  = getCookie();
  
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
